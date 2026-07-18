e936d2badb4a3e4d7b8fe29380dfeb7f6aeb78dc
Sandramar-mdq <69278516+Sandramar-mdq@users.noreply.github.com>
2026-07-18 12:37:36 -0300

    Update iniciar_servidor.bat

--- iniciar_servidor.bat
+++ iniciar_servidor.bat
@@ -1,3 +1,10 @@
 @echo off
-call .\.venv\Scripts\activate
-python -m uvicorn src.main:app --reload --reload-dir src
+title Marea Activa - Servidor Backend
+
+:: 1. Abre automáticamente el navegador en la documentación interactiva de Swagger UI
+start "" "http://127.0.0.1:8000/docs"
+
+:: 2. Levanta el servidor usando el Python correcto del entorno virtual de forma explícita
+.\.venv\Scripts\python -m uvicorn src.main:app --reload --reload-dir src
+
+pause
